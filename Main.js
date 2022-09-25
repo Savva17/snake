16e6e4cd2e2e91ce32c5820dcbdc014d0bf307b7
Maks <maxsavinec@gmail.com>
2022-09-25 22:07:37 +0300

    a2

--- Main.js
+++ Main.js
@@ -60,12 +60,12 @@ class Snake {
         // инициялизация змейки
         // например размер змейки, цвет, начальное позиционирование
         // и другие параметры
-
         this.nextY;//координаты головы
         this.nextX;//координаты головы
         this.auto; // интервал
         this.time = 500;// начальная скорость змейки
         this.res = 0;
+
         document.querySelector(".x" + 5 + ".y" + 5).classList.add("snake");// начальные координаты змейки
         document.querySelector(".x" + 5 + ".y" + 6).classList.add("snaketail");// начальные координаты тела
 
@@ -78,16 +78,17 @@ class Snake {
 
     death() {
         //логика смерти змейки
-       
-        for (this.a = 0; this.a < this.arrYX.length; this.a++){
+
+        for (this.a = 0; this.a < this.arrYX.length; this.a++) {
             // если координаты головы совпадут с координатами тела змейки
-            if (this.nextY === this.arrYX[this.a][0] && this.nextX === this.arrYX[this.a][1]){
+            if (this.nextY === this.arrYX[this.a][0] && this.nextX === this.arrYX[this.a][1]) {
                 this.route = 0;
                 document.querySelector(".snake").classList.remove("snake");
                 clearInterval(this.auto)
-                for (this.i = 0; this.i < 1+ main.score._score; this.i++) {
+                for (this.i = 0; this.i < 1 + main.score._score; this.i++) {
 
-                    document.querySelector(".snaketail").classList.remove("snaketail");                }
+                    document.querySelector(".snaketail").classList.remove("snaketail");
+                }
                 ;
             }
         }
@@ -96,16 +97,15 @@ class Snake {
     update() {
         // логика обновления змейки
         // условия увеличения скорости змейки
-        if(this.res === 5) {
+        if (this.res === 5) {
             this.time -= 50;
             this.res = 0;
         }
     }
 
     draw() {
-        // логика отрисовки змейки    
 
-        
+        // логика отрисовки змейки    
         this.auto = setInterval(() => {
             if (this.route === 'up') {
                 this.x = +document.querySelector(".snake").dataset.x;
@@ -137,10 +137,11 @@ class Snake {
                 main.apple.getPosition(this.nextY, this.nextX)
             }
             this.death()
+            // удаление головы змейки с поля
+            document.querySelector(".snake").classList.remove("snake");
 
-            document.querySelector(".snake").classList.remove("snake");   // удаление головы змейки с поля
-
-            document.querySelector(".x" + this.nextX + ".y" + this.nextY).classList.add("snake"); // добавление змейки на поле по кординатам
+            // добавление змейки на поле по кординатам
+            document.querySelector(".x" + this.nextX + ".y" + this.nextY).classList.add("snake");
 
             // добавление координат головы в массив и удаление предыдущего хвоста на одну клетку
             this.arrYX.unshift([this.y, this.x])
@@ -149,23 +150,22 @@ class Snake {
             document.querySelector(".x" + this.arrYX.slice(-1)[0][1] + ".y" + this.arrYX.slice(-1)[0][0]).classList.remove("snaketail");
 
             //  удаление последних координат в массиве. длина массива зависит от сьеденых яблок
-            this.arrYX.splice(1+main.score._score, 2)
+            this.arrYX.splice(1 + main.score._score, 2)
 
             // цикл прорисовки хвоста змейки
-            for (this.i = 0; this.i < 1+ main.score._score; this.i++) {
+            for (this.i = 0; this.i < 1 + main.score._score; this.i++) {
 
                 document.querySelector(".x" + this.arrYX[this.i][1] + ".y" + this.arrYX[this.i][0]).classList.add("snaketail");
             }
 
         }, this.time)
-        
+
     }
 
-    control(e) {
+    control(e, a) {
         // логика управления змейкой
         // обработка кнопок на клавиатуре
         this.route;
-
 
 
         if (e.code === 'ArrowUp' && this.route !== 'down') {
@@ -200,7 +200,7 @@ class Snake {
 class Apple {
     constructor() {
         //инициализация параметров яблока, начальные координаты
-        this.appleX = 2 ;// начальные координаты яблока
+        this.appleX = 2;// начальные координаты яблока
         this.appleY = 2;// начальные координаты яблока
         this.d = 0;
         // добавление яблока на поле
@@ -212,7 +212,7 @@ class Apple {
         //отрисовка яблока
         this.appleX = Math.floor(Math.random() * main.field.amountSquare);
         this.appleY = Math.floor(Math.random() * main.field.amountSquare);
-        
+
         ;
 
         //добавление координат яблока в массив
@@ -226,8 +226,9 @@ class Apple {
 
             for (this.i = 0; this.i < main.snake.arrYX.length; this.i++) {
 
-                if (main.snake.arrYX[this.i][0] !== this.arrApple[0] || main.snake.arrYX[this.i][1] !== this.arrApple[1] && main.snake.nextY !== this.arrApple[0] && main.snake.nextX !== this.arrApple[1]) {
-
+                if (main.snake.arrYX[this.i][0] !== this.arrApple[0] && main.snake.arrYX[this.i][1] !== this.arrApple[1] && main.snake.nextY !== this.arrApple[0] && main.snake.nextX !== this.arrApple[1]) {
+                    console.log(main.snake.arrYX[this.i][0], main.snake.arrYX[this.i][1])
+                    console.log(this.arrApple[0], this.arrApple[1])
                     document.querySelector('.apple').classList.remove('apple')
                     return document.querySelector('.x' + this.appleX + '.y' + this.appleY).classList.add('apple');
 
@@ -241,7 +242,7 @@ class Apple {
     }
 
     getPosition(a, b) {
-        
+
         //получение новой позиции яблока на поле
         if (b == this.appleX && a == this.appleY) {
             main.snake.res++
@@ -274,8 +275,8 @@ class Score {
         //увеличивать количество очков
         //и перерисовывать табло
         this._score += 1;
-        if (this._score>localStorage.getItem('score')){
-            localStorage.setItem('score',this._score)
+        if (this._score > localStorage.getItem('score')) {
+            localStorage.setItem('score', this._score)
         }
         this.draw();
     }
@@ -291,12 +292,34 @@ let main = new Main();
 main.score.draw()
 main.apple.draw()
 
+
 document.addEventListener('keydown', function (e) {
     main.snake.control(e)
 })
 let reset = document.querySelector('.reset')
 reset.addEventListener('click', function () {
     location.reload();
+})
+let buttonUp = document.querySelector('.up');
+let buttonDown = document.querySelector('.down');
+let buttonRight = document.querySelector('.right');
+let buttonLeft = document.querySelector('.left');
+
+buttonUp.addEventListener('click', function (e) {
+    e.code = 'ArrowUp'
+    main.snake.control(e)
+})
+buttonDown.addEventListener('click', function (e) {
+    e.code = 'ArrowDown'
+    main.snake.control(e)
+})
+buttonRight.addEventListener('click', function (e) {
+    e.code = 'ArrowRight'
+    main.snake.control(e)
+})
+buttonLeft.addEventListener('click', function (e) {
+    e.code = 'ArrowLeft'
+    main.snake.control(e)
 })
 
 // export default Main;
